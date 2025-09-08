f223730294ab47ef3f76feae403a7fff1f7badd4
jaxrkindred <jaxonkindred@gmail.com>
2025-09-07 21:18:14 -0600

    ui is working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,16 +9,11 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={GeistSans.className}>
       <body>{children}</body>
     </html>
   );
